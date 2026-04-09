43f49be196ed6681e47ac3f0bbf6ae54964e1bc9
palthyasachin <palthyasachin45@gmail.com>
2026-04-09 17:13:20 +0100

    implement course discovery page with associated models and services

--- SkillStreak_App/app/models/CourseModel.js
+++ SkillStreak_App/app/models/CourseModel.js
@@ -5,6 +5,16 @@ class courseModel {
         const [courses] = await db.execute('SELECT * FROM courses');
         return courses;
     }
+
+    async getAllJobs() {
+        const [jobs] = await db.execute(`
+            SELECT j.*, c.course_title 
+            FROM jobs j
+            LEFT JOIN courses c ON j.related_course = c.course_id
+            ORDER BY j.posted_at DESC
+        `);
+        return jobs;
+    }
 }
 
 module.exports = new courseModel();
--- SkillStreak_App/app/services/CourseService.js
+++ SkillStreak_App/app/services/CourseService.js
@@ -3,9 +3,11 @@ const courseModel = require('../models/courseModel');
 class courseService {
     async getcourseData() {
         const courses = await courseModel.getAllCourses();
+        const jobs = await courseModel.getAllJobs();
         return {
             title: 'Explore course | SkillStreak',
-            courses
+            courses,
+            jobs
         };
     }
 }
